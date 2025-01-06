821614f720d4c253621e9465763329e89147f787
DESKTOP-KM47ILD\Usuário <tjusto2409@gmail.com>
2025-01-06 09:46:46 -0300

    Create website

---
+++ src/js/index.js
@@ -0,0 +1,11 @@
+import '../styles/_bootstrap-grid.scss';
+import '../styles/_colors.scss';
+import '../styles/_mixins.scss';
+import '../styles/_reset.scss';
+import '../styles/_typography.scss';
+import '../styles/main.scss';
+
+import '../images/apple-touch-icon.png';
+import '../images/favicon-16x16.png';
+import '../images/favicon-32x32.png';
+import '../images/favicon.ico';
