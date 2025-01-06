5c3c49032c739c1809d01f9af21c9de7359a17fc
DESKTOP-KM47ILD\Usuário <tjusto2409@gmail.com>
2025-01-06 10:48:21 -0300

    update site

---
+++ dist/bundle.199c54bb6a2491f5cdd4.js
@@ -0,0 +1,64 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	// The require scope
+/******/ 	var __webpack_require__ = {};
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/global */
+/******/ 	(() => {
+/******/ 		__webpack_require__.g = (function() {
+/******/ 			if (typeof globalThis === 'object') return globalThis;
+/******/ 			try {
+/******/ 				return this || new Function('return this')();
+/******/ 			} catch (e) {
+/******/ 				if (typeof window === 'object') return window;
+/******/ 			}
+/******/ 		})();
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/publicPath */
+/******/ 	(() => {
+/******/ 		var scriptUrl;
+/******/ 		if (__webpack_require__.g.importScripts) scriptUrl = __webpack_require__.g.location + "";
+/******/ 		var document = __webpack_require__.g.document;
+/******/ 		if (!scriptUrl && document) {
+/******/ 			if (document.currentScript && document.currentScript.tagName.toUpperCase() === 'SCRIPT')
+/******/ 				scriptUrl = document.currentScript.src;
+/******/ 			if (!scriptUrl) {
+/******/ 				var scripts = document.getElementsByTagName("script");
+/******/ 				if(scripts.length) {
+/******/ 					var i = scripts.length - 1;
+/******/ 					while (i > -1 && (!scriptUrl || !/^http(s?):/.test(scriptUrl))) scriptUrl = scripts[i--].src;
+/******/ 				}
+/******/ 			}
+/******/ 		}
+/******/ 		// When supporting browsers where an automatic publicPath is not supported you must specify an output.publicPath manually via configuration
+/******/ 		// or pass an empty string ("") and set the __webpack_public_path__ variable from your code to use your own logic.
+/******/ 		if (!scriptUrl) throw new Error("Automatic publicPath is not supported in this browser");
+/******/ 		scriptUrl = scriptUrl.replace(/#.*$/, "").replace(/\?.*$/, "").replace(/\/[^\/]+$/, "/");
+/******/ 		__webpack_require__.p = scriptUrl;
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+
+;// ./src/images/apple-touch-icon.png
+const apple_touch_icon_namespaceObject = __webpack_require__.p + "images/aca303504aa44ceab8d6.png";
+;// ./src/images/favicon-16x16.png
+const favicon_16x16_namespaceObject = __webpack_require__.p + "images/85c0194090ef2f740d2c.png";
+;// ./src/images/favicon-32x32.png
+const favicon_32x32_namespaceObject = __webpack_require__.p + "images/288610f364a8ee3c6513.png";
+;// ./src/images/favicon.ico
+const favicon_namespaceObject = __webpack_require__.p + "images/f0bbe36e299596d5289d.ico";
+;// ./src/js/index.js
+
+
+
+
+
+
+
+
+
+
+/******/ })()
+;
